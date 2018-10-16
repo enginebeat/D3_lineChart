312272abca33b9934ab0594ce53dccbf10c20589
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-10-16 16:16:43 +0100

    played with the original graph to change domain calculations

--- D3_lineChart_v1/index.js
+++ D3_lineChart_v1/index.js
@@ -6,6 +6,12 @@ for(var i = 0; i <= 20; i++){
 }
 //console.log(tData);
 
+var tData1 = [];
+
+for(var i = 0; i <= 20; i++){
+    tData1.push({sampleNumber: i, sampleValue: Math.round( Math.random() * 10 ) });
+}
+
 
 /* 
     Loading Data from the API When DOM Content has been loaded
@@ -14,20 +20,31 @@ for(var i = 0; i <= 20; i++){
 document.addEventListener("DOMContentLoaded", (event) => {
     
     console.log(tData);
-    drawChart(tData);
+    drawChart(tData, tData1);
     //drawChart(parsedData);
         
 });
 
-function drawChart(data) {
-    var svgWidth = 600, svgHeight = 400;
+function drawChart(data, data1) {
+    var svgWidth = 700, svgHeight = 500;
     var margin = { top: 20, right: 20, bottom: 30, left: 50};
     var width = svgWidth - margin.left -margin.right;
     var height = svgHeight - margin.top - margin.bottom;
 
+    var graph1 = d3.select('#graph1')
+        .append('svg')
+        .attr('class', 'svg1');
+
+    graph1  .attr('width', svgWidth)    // setting size of the svg window by giving the container width & height
+            .attr('height', svgHeight);
+
+    var svg = d3.select('.svg1');
+
+    /*
     var svg = d3.select('svg')
         .attr('width', svgWidth)
         .attr('height', svgHeight);
+    */
     
     var g = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
@@ -41,8 +58,20 @@ function drawChart(data) {
     var line = d3.line()
         .x((d) => { return x(d.sampleNumber) })
         .y((d) => { return y(d.sampleValue) })
-        x.domain(d3.extent(data, (d) => { return d.sampleNumber }));    //extent returns the min and max value to create the scales.
-        y.domain(d3.extent(data, (d) => { return d.sampleValue }));
+        //x.domain(d3.extent(data, (d) => { return d.sampleNumber }));    //extent returns the min and max value to create the scales.
+        //y.domain(d3.extent(data, (d) => { return d.sampleValue }));
+
+    var line1 = d3.line()
+        .x((d) => { return x(d.sampleNumber) })
+        .y((d) => { return y(d.sampleValue) })
+        //x.domain(d3.extent(data1, (d) => { return d.sampleNumber }));    //extent returns the min and max value to create the scales.
+        //y.domain(d3.extent(data1, (d) => { return d.sampleValue }));
+
+
+    var allValues = data.concat(data1);
+    console.log(allValues);
+    x.domain(d3.extent(allValues, (d) => { return d.sampleNumber }));    //extent returns the min and max value to create the scales.
+    y.domain(d3.extent(allValues, (d) => { return d.sampleValue }));
 
     g.append('g')
         .attr('transform', 'translate(0,' + height + ')')
@@ -69,4 +98,13 @@ function drawChart(data) {
         .attr('stroke-width', 1.5)
         .attr('d', line);
 
+    g.append('path')
+        .datum(data1)
+        .attr('fill', 'none')
+        .attr('stroke', 'red')
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+        .attr('stroke-width', 1.5)
+        .attr('d', line1);
 }
+
